b3efa3e00fcd2d13f0ecc6ddb156674466cc8d63
Nic <nic.dan.costea@gmail.com>
2022-10-11 16:58:43 +0200

    day 12 homework

---
+++ homework/index.js
@@ -0,0 +1,14 @@
+// how to inject a new container
+// create one new div inside another new div
+// and inject after the second div with id=second-div
+const injectSpecificPosition = () => {
+  const newContainer = document.createElement("div");
+  newContainer;
+};
+
+// upon clicking the button we take the value of the input
+// and do something
+// we create a function that we use on the onclick attribute
+
+// ...on the onchange attribute
+//
